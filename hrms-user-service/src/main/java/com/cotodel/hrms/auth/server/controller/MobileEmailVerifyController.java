33e964a3878d8403b2666480941801c2adb20d1b
atulyadavayo <atulyadavayo@gmail.com>
2024-02-14 17:46:30 +0530

    email controller change

--- hrms-user-service/src/main/java/com/cotodel/hrms/auth/server/controller/MobileEmailVerifyController.java
+++ hrms-user-service/src/main/java/com/cotodel/hrms/auth/server/controller/MobileEmailVerifyController.java
@@ -67,7 +67,7 @@ public class MobileEmailVerifyController {
 	    		String authToken=request.getHeader("Authorization");
 	    		userEntity=userService.checkUserMobile(userReq.getMobile());
 	    		if(userEntity!=null && userEntity.getStatus()==MessageConstant.ONE ) {
-	    			response=userService.sendSmsOtp(authToken,userReq.getMobile());
+	    			//response=userService.sendSmsOtp(authToken,userReq.getMobile());
 	    			response="{\"errCode\":\"\",\"errDes\":\"\",\"txn\":\"NHA:53029a89-ae73-4e52-bdfc-0f47d237a6fc\",\"ts\":\"2024-02-14T15:12:24.240+05:24\",\"status\":\"true\"}";
 	    			if(!ObjectUtils.isEmpty(response)) {
 
